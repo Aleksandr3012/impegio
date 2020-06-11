4ebeda32c1a6f1e8b8275a2301b6554b8dbc2745
Aleksandr3012 <flaming541@gmail.com>
2020-06-11 15:36:31 +0300

    impressum

---
+++ sourse/pug/blocks/sResponsibility/_sResponsibility.js
@@ -0,0 +1,13 @@
+let sResponsibilityVue = new Vue({
+		el: '#sResponsibility',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
