7223ade3354aceccc7fa754b76b5c3435e395039
luckyone <luckyone1221@gmail.com>
2020-06-12 14:57:57 +0300

    fix img sJobFinden

---
+++ sourse/pug/blocks/sJobFindenSlider/_sJobFindenSlider.js
@@ -0,0 +1,13 @@
+let sJobFindenSliderVue = new Vue({
+		el: '#sJobFindenSlider',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
